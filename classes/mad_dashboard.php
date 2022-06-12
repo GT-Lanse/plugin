65587674e26306078d4b489ed41d7f0e76e0c3c9
matheusfrancisco <matheusmachadoufsc@gmail.com>
2022-06-12 13:13:08 -0300

    add amd server and webservice

--- classes/mad_dashboard.php
+++ classes/mad_dashboard.php
@@ -3,40 +3,149 @@ namespace block_mad2api;
 //use helpers\S3;
 defined('MOODLE_INTERNAL') || die();
 
+// require(__DIR__.'/../../config.php');
+
+require_once("$CFG->libdir/externallib.php");
+use external_api;
+use external_function_parameters;
+use external_single_structure;
+use external_value;
+use external_multiple_structure;
+
 # More enhancements will come later as the NN dev continues
-class mad_dashboard{
+class mad_dashboard extends external_api {
   function __construct(){}
+
+  public static function enable_parameters() {
+    return new external_function_parameters([
+      'courseId' => new external_value(PARAM_INT, 'Course id', VALUE_DEFAULT, 0),
+    ]);
+  }
+
+  public static function enable_returns() {
+    return new external_multiple_structure(
+      new external_single_structure(
+        array(
+          'enabled' => new external_value(PARAM_BOOL, VALUE_REQUIRED)
+        )
+      )
+    );
+  }
 
   public static function enable($courseId)
   {
     global $DB, $USER, $COURSE;
+
+    $params = self::validate_parameters(self::enable_parameters(),
+      array(
+        'courseId' => $courseId,
+      )
+    );
+
+    $dashboard_setting = $DB->get_record(
+      "mad2api_dashboard_settings",
+      array('user_id' => $USER->id, 'course_id' => $courseId)
+    );
+
+    if (isset($dashboard_setting) && $dashboard_setting->is_enabled == 1) {
+      return array(['enabled' => true]);
+    }
+
     $database_response = false;
     $tokenHex = bin2hex(random_bytes(16));
-    $response = self::api_enable_call($courseId, $tokenHex);
-    if(!property_exists($response, 'plugin-info')){
+    $response = self::api_enable_call($params['courseId'], $tokenHex);
+
+    if (!property_exists($response, 'plugin-info')) {
       return;
     }
-    $row_check = $DB->get_record("mad2api_dashboard_settings", ['user_id' => $USER->id, 'course_id' => $courseId]);
-    if (!$row_check) {
-      $record = (object) array('user_id' => $USER->id,
-                               'created_at' => date('Y-m-d H:i:s'),
-                               'updated_at' => date('Y-m-d H:i:s'),
-                               'last_log_date' => date('Y-m-d'),
-                               'course_id' => $courseId,
-                               'is_enabled' => 1,
-                               'token' => $tokenHex,
-                             );
-      $database_response = $DB->insert_record('mad2api_dashboard_settings', $record, false);
-      self::upload_logs($courseId);
+
+    $record = array(
+      'user_id' => $USER->id,
+      'created_at' => date('Y-m-d H:i:s'),
+      'updated_at' => date('Y-m-d H:i:s'),
+      'last_log_date' => date('Y-m-d'),
+      'course_id' => intval($params['courseId']),
+      'is_enabled' => 1,
+      'token' => $tokenHex
+    );
+
+    if (isset($dashboard_setting->id)) {
+      $record['id'] = $dashboard_setting->id;
+      $database_response = $DB->update_record('mad2api_dashboard_settings', $record, false);
     } else {
-      $update_grade_value = "
-        UPDATE mdl_mad2api_dashboard_settings
-        SET is_enabled=1
-        WHERE user_id = $USER->id AND course_id = $courseId
-      ";
-      $database_response = $DB->execute($update_grade_value);
+      $database_response = $DB->insert_record('mad2api_dashboard_settings', $record, false);
     }
-    return $database_response;
+
+    self::upload_logs($params['courseId']);
+
+    return array(['enabled' => $database_response]);
+  }
+
+  public static function disable_parameters() {
+    return new external_function_parameters([
+      'courseId' => new external_value(PARAM_INT, 'Course id', VALUE_DEFAULT, 0),
+    ]);
+  }
+
+  public static function disable_returns() {
+    return new external_multiple_structure(
+      new external_single_structure(
+        array(
+          'disabled' => new external_value(PARAM_BOOL, VALUE_REQUIRED)
+        )
+      )
+    );
+  }
+
+  public static function disable($courseId)
+  {
+    global $USER, $DB;
+
+    $params = self::validate_parameters(self::enable_parameters(),
+      array(
+        'courseId' => $courseId,
+      )
+    );
+
+    $data = array(
+      'user_id' => $USER->id,
+      'course_id' => $courseId,
+      'updated_at' => date('Y-m-d H:i:s'),
+      'is_enabled' => 0
+    );
+
+    $database_response = false;
+    $dashboard_setting = $DB->get_record(
+      "mad2api_dashboard_settings",
+      ['user_id' => $USER->id, 'course_id' => $courseId]
+    );
+
+    if (isset($dashboard_setting->id)) {
+      $data['id'] = $dashboard_setting->id;
+      $database_response = $DB->update_record('mad2api_dashboard_settings', $data);
+    }
+
+    return array(['disabled' => $database_response]);
+  }
+
+  public static function is_current_user_course_teacher($contextid) {
+    global $USER;
+
+    $is_teacher = false;
+
+    foreach (self::get_user_roles($USER->id, $contextid) as $user_role) {
+      if ($user_role->roleid == 4 || $user_role->roleid == 3) {
+        $is_teacher = true;
+      }
+    }
+
+    return $is_teacher;
+  }
+
+  public static function get_user_roles($userid, $contextid) {
+    global $COURSE, $DB;
+
+    return $DB->get_records('role_assignments', array('contextid' => $contextid, 'userid' => $userid));
   }
 
   public static function api_enable_call($courseId, $tokenHex){
@@ -59,26 +168,15 @@ class mad_dashboard{
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));  //Post Fields
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $headers = [
-        'accept: application/json',
-        'Content-Type: application/json',
+      'accept: application/json',
+      'Content-Type: application/json',
     ];
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     $server_output = curl_exec($ch);
     curl_close($ch);
+
     return json_decode($server_output);
   }
-
-  public static function disable($courseId)
-  {
-    global $USER, $DB;
-    $update_query = "
-      UPDATE mdl_mad2api_dashboard_settings
-      SET is_enabled=0
-      WHERE user_id=$USER->id AND course_id=$courseId
-    ";
-    return $DB->execute($update_query);
-  }
-
 
   public static function get_last_log_date()
   {
@@ -108,7 +206,7 @@ class mad_dashboard{
     $organization = get_config('mad2api', 'organization');
     $course_settings = $DB->get_record("mad2api_dashboard_settings", ['user_id' => $USER->id, 'course_id' => $courseId]);
 
-    $s3 = new \S3("KEY", "TOKEN", false);
+    $s3 = new \S3("AKIARLANPF2DURY6V6P7", "eSFF5ojTveXsvMZTaIQT1pP3OoEEFfIi6PXYELvf", false);
     // echo "S3::listBuckets(): ".print_r($s3->listBuckets(), 1)."\n";
     // return;
     $logs_query = '
@@ -145,13 +243,22 @@ class mad_dashboard{
             fclose($fp);
         }
      }
+    str_putcsv($logs);
     $s3->putObject(file_get_contents('./temp.csv'), 'futurogfp-documents', "unprocessed/$organization/$campus/$course_settings->token/$courseId.csv", \S3::ACL_PRIVATE, array(), array('Content-Type' => 'text/csv'));
   }
 
-  public static function get_dashboard_status()
+  public static function get_dashboard_status($courseId)
   {
-    global $USER, $DB, $COURSE;
-    return $DB->get_record("mad2_user_prediction_setting", array('user_id'=>$USER->id, 'course_id'=>$COURSE->id), $fields='is_enabled');
+    global $USER, $DB;
+
+    return $DB->get_record(
+      "mad2api_dashboard_settings",
+      array(
+        'user_id' => $USER->id,
+        'course_id' => $courseId,
+        'is_enabled' => 1
+      )
+    );
   }
 
   public static function scheduled_log(){
@@ -166,5 +273,4 @@ class mad_dashboard{
       self::upload_logs($active_course->course_id);
     }
   }
-
 }
