276253ebd8c1d3f80aaff0224aa35e56041e79a0
Vinicius Ramos <professor.vinicius.ramos@gmail.com>
2026-08-18 07:38:19 -0300

    Plugin version updated

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026063000;
-$plugin->release = 'v3.0.2';
+$plugin->version   = 2026081800;
+$plugin->release = 'v3.0.3';
 $plugin->requires  = 2012112900;
 $plugin->component = 'block_mad2api';
 $plugin->maturity  = MATURITY_STABLE;
